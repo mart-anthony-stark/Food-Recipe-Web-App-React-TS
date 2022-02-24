cf5361b5901051825f56fe1f15753c0c74c532f7
mart-anthony-stark <martsalazar0@gmail.com>
2022-02-24 19:09:02 +0800

    ADD: navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div className="App">
-
+      <Navbar title="Recipe"></Navbar>
     </div>
   )
 }
