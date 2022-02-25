5df1cc8b1d64e6b0d06c3acc15d20c8b1523d2ca
mart-anthony-stark <martsalazar0@gmail.com>
2022-02-25 10:17:40 +0800

    add: banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Navbar from './components/navbar/Navbar'
+import Banner from './components/banner/Banner'
 
 function App() {
   return (
     <div className="App">
-      <Navbar></Navbar>
+      <Navbar />
+      <Banner/>
     </div>
   )
 }
