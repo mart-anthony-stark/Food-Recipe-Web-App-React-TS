03ac8c0010b09ab86d3f445359367a558d4f237e
mart-anthony-stark <martsalazar0@gmail.com>
2022-02-26 19:20:15 +0800

    add: search box

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -1,7 +1,11 @@
 import './banner.css'
 
 const Banner = () =>{
-    return <div className="banner">
+    return <div className="banner center-items">
+        <div className="search-box">
+            <input type="text" placeholder="Search Recipe"/>
+            <button className="center-items">Search</button>
+        </div>
     </div>
 }
 export default Banner
