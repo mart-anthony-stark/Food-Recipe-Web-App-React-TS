867f31650642caba0bb1f8bf2beb54b92143665f
mart-anthony-stark <martsalazar0@gmail.com>
2022-02-26 19:39:24 +0800

    add: category

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -1,12 +1,20 @@
+import { Suspense, lazy } from 'react';
 import './home.css'
 import Banner from "../../components/banner/Banner";
 import Navbar from "../../components/navbar/Navbar";
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+
 
 const Home = () => {
   return (
     <>
       <Navbar />
-        <Banner />
+      <Banner />
+      <div className="categories">
+        <div className="category">
+          <LazyLoadImage src="/src/assets/img/steak.jpg" effect="blur"/>
+        </div>
+      </div>
     </>
   );
 };
