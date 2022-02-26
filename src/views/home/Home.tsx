403da18a1b8fb59abd4ceb6b21b9119ce6f19aab
mart-anthony-stark <martsalazar0@gmail.com>
2022-02-26 19:52:32 +0800

    add: category

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -3,6 +3,7 @@ import './home.css'
 import Banner from "../../components/banner/Banner";
 import Navbar from "../../components/navbar/Navbar";
 import { LazyLoadImage } from 'react-lazy-load-image-component';
+import Heading from '../../components/Heading';
 
 
 const Home = () => {
@@ -10,9 +11,16 @@ const Home = () => {
     <>
       <Navbar />
       <Banner />
+      <Heading title="Categories" />
       <div className="categories">
         <div className="category">
           <LazyLoadImage src="/src/assets/img/steak.jpg" effect="blur"/>
+        </div>
+        <div className="category">
+          <LazyLoadImage src="/src/assets/img/side-dish.jpg" effect="blur"/>
+        </div>
+        <div className="category">
+          <LazyLoadImage src="/src/assets/img/desert.jpg" effect="blur"/>
         </div>
       </div>
     </>
