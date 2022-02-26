15ad82688062e5de82886d6a53fed7ef8f347431
mart-anthony-stark <martsalazar0@gmail.com>
2022-02-26 19:07:34 +0800

    moved dir

---
+++ src/views/home/Home.tsx
@@ -0,0 +1,15 @@
+import Banner from "../../components/banner/Banner";
+import Navbar from "../../components/navbar/Navbar";
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <main>
+        <Banner />
+      </main>
+    </>
+  );
+};
+
+export default Home;
