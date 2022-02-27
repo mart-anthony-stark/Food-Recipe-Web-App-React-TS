91ee8e433430e9c5e22486c19c56f66b2cd4ccbb
mart-anthony-stark <martsalazar0@gmail.com>
2022-02-27 09:59:18 +0800

    add: navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
-import { lazy, Suspense } from 'react'
-import './App.css'
-import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom'
-import Navbar from './components/navbar/Navbar'
-import Banner from './components/banner/Banner'
-const Home = lazy(()=> import('./views/home/Home'))
+import { lazy, Suspense } from "react";
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Navbar from "./components/navbar/Navbar";
+const Home = lazy(() => import("./views/home/Home"));
 
 function App() {
   return (
     <div className="App">
       <main>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Navigate to="/home"/>}/>
-          <Route path="/home" element={<Suspense fallback={<>...</>}><Home/></Suspense>}/>
-        </Routes>
-      </Router>
-      </main>
+        <Navbar />
 
+        <Router>
+          <Routes>
+            <Route path="/" element={<Navigate to="/home" />} />
+            <Route
+              path="/home"
+              element={
+                <Suspense fallback={<>...</>}>
+                  <Home />
+                </Suspense>
+              }
+            />
+          </Routes>
+        </Router>
+      </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
