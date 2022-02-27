438fd6d11a98cf9c73b1b44859d8f1f456141c3e
mart-anthony-stark <martsalazar0@gmail.com>
2022-02-27 09:45:35 +0800

    category mapping

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -1,24 +1,24 @@
-import { Suspense, lazy } from 'react';
-import './home.css'
+import { Suspense, lazy } from "react";
+import "./home.css";
 import Banner from "../../components/banner/Banner";
 import Navbar from "../../components/navbar/Navbar";
-import { LazyLoadImage } from 'react-lazy-load-image-component';
-import Heading from '../../components/Heading';
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import Heading from "../../components/Heading";
 
 const categories = [
   {
-    name: 'Main course',
-    src: '/src/assets/img/steak.jpg'
+    name: "Main course",
+    src: "/src/assets/img/steak.jpg",
   },
   {
-    name: 'Side dish',
-    src: '/src/assets/img/side-dish.jpg'
+    name: "Side dish",
+    src: "/src/assets/img/side-dish.jpg",
   },
   {
-    name: 'Desserts',
-    src: '/src/assets/img/dessert.jpg'
+    name: "Desserts",
+    src: "/src/assets/img/dessert.jpg",
   },
-]
+];
 
 const Home = () => {
   return (
@@ -27,15 +27,11 @@ const Home = () => {
       <Banner />
       <Heading title="Categories" />
       <div className="categories">
-        <div className="category">
-          <LazyLoadImage src="/src/assets/img/steak.jpg" effect="blur"/>
-        </div>
-        <div className="category">
-          <LazyLoadImage src="/src/assets/img/side-dish.jpg" effect="blur"/>
-        </div>
-        <div className="category">
-          <LazyLoadImage src="/src/assets/img/desert.jpg" effect="blur"/>
-        </div>
+        {categories.map(({ name, src }) => (
+          <div className="category">
+            <LazyLoadImage src={src} effect="blur" />
+          </div>
+        ))}
       </div>
     </>
   );
