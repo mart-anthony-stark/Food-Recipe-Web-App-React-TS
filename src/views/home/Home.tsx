f09e360eaed2a52415242db34ea2ecba29a3ac36
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-02 10:16:12 +0800

    add description

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -7,6 +7,7 @@ import Heading from "../../components/Heading";
 
 interface Category {
   name: string;
+  description: string;
   src: string;
   link: string;
 }
@@ -14,16 +15,22 @@ interface Category {
 const categories: Category[] = [
   {
     name: "Main course",
+    description:
+      "Dinner usually consists of just one main course, often a meat dish with potatoes and a vegetable or salad.",
     src: "/src/assets/img/steak.jpg",
     link: "/categories/main-course",
   },
   {
     name: "Side dish",
+    description:
+      "Essentially any food served on the side of the plate or to the side of the main plate.",
     src: "/src/assets/img/side-dish.jpg",
     link: "/categories/side-dish",
   },
   {
     name: "Desserts",
+    description:
+      "Usually sweet course or dish (as of pastry or ice cream) usually served at the end of a meal.",
     src: "/src/assets/img/dessert.jpg",
     link: "/categories/dessert",
   },
@@ -35,16 +42,17 @@ const Home = () => {
       <Banner />
       <Heading title="Categories" />
       <div className="categories">
-        {categories.map(({ name, src, link }) => (
+        {categories.map(({ name, description, src, link }) => (
           <Link to={link}>
             <div className="category center-items">
               <div className="image">
-                <div className="overlay">
+                <div className="overlay center-items">
                   <span>See Recipes</span>
                 </div>
                 <LazyLoadImage src={src} effect="blur" />
               </div>
               <h2 className="category-name">{name}</h2>
+              <p className="description">{description}</p>
             </div>
           </Link>
         ))}
