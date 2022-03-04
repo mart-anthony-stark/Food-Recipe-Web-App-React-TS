d37ffdabe33cd03d18f8fec1bbc9558f5b88ab3f
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-04 14:55:52 +0800

    add recipe types

--- src/components/featured/Featured.tsx
+++ src/components/featured/Featured.tsx
@@ -1,9 +1,20 @@
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from "react";
+
+interface Recipe {
+  id: number;
+  title: string;
+  sourceUrl: string;
+  image: string;
+  imageType: string;
+  summary: string;
+  dishTypes: string[];
+  instructions: string;
+  analyzedInstructions: Array;
+}
 
 const Featured = () => {
-  return (<div className="featured">
-
-  </div>);
+  const [recipes, setRecipes] = useState();
+  return <div className="featured"></div>;
 };
 
 export default Featured;
