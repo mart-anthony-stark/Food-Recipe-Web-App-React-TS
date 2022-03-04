f4592a1d76fd2fea97318dabaa64b90645039b31
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-04 10:25:48 +0800

    add burger menu

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,21 +1,28 @@
-import Logo from '../../assets/img/logo.png'
-import './navbar.css'
-const Navbar = () =>{
-    return (<nav>
-        <div className="left">
+import Logo from "../../assets/img/logo.png";
+import "./navbar.css";
+const Navbar = () => {
+  return (
+    <nav>
+      {/* <div className="left">
             <a href="#">Recipes</a>
             <a href="#">Ingredients</a>
             <a href="#">Meal Planning</a>
+        </div> */}
+      <div className="left">
+        <div className="menu">
+          <div className="bar"></div>
         </div>
-        <div className="logo">
-            <img src={Logo} alt="Kitcheen" />
-        </div>
-        <div className="right">
-            <a href="#">About</a>
-            <a href="#">Service</a>
-            <a href="#">Blog</a>
-        </div>
-    </nav>)
-}
+      </div>
+      <div className="logo">
+        <img src={Logo} alt="Kitcheen" />
+      </div>
+      <div className="right">
+        <a href="#">About</a>
+        <a href="#">Service</a>
+        <a href="#">Blog</a>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
